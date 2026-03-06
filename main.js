2359d86644a4ba2a3587de1a4deec4e60fe7583c
Rich Williams <r.d.williams@live.co.uk>
2026-03-06 09:16:17 +0000

    updated menu colour on small screens to orange

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelector('.nav-links');
+const nav = document.querySelector('nav');
 
 navToggle.addEventListener('click', function() {
   navLinks.classList.toggle('is-open');
   navToggle.classList.toggle('is-open');
+  nav.classList.toggle('is-open');
 });
