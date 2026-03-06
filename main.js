a0c9ada0fb020ee4b8bd1969e380b37008c31922
Rich Williams <r.d.williams@live.co.uk>
2026-03-06 10:13:50 +0000

    updated transition some more menu transition to make it smoother and make nav bar sticky

--- main.js
+++ main.js
@@ -2,8 +2,18 @@ const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelector('.nav-links');
 const nav = document.querySelector('nav');
 
+/*controls the menu and burger icon on small screens*/
 navToggle.addEventListener('click', function() {
   navLinks.classList.toggle('is-open');
   navToggle.classList.toggle('is-open');
   nav.classList.toggle('is-open');
+});
+
+/*adds in class to CSS that shows shadow under the nav bar on scroll*/
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 0) {
+    nav.classList.add('is-scrolled');
+  } else {
+    nav.classList.remove('is-scrolled');
+  }
 });
